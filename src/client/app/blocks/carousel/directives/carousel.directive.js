a608b739d62a7423983c2119ab1408c023f17bee
swathi uppu <swathiu@virtusa.com>
2017-01-13 14:55:07 +0530

    latest code

--- src/client/app/blocks/carousel/directives/carousel.directive.js
+++ src/client/app/blocks/carousel/directives/carousel.directive.js
@@ -16,10 +16,10 @@
     };
 
     function carouselController () {
-      this.slides = [{"image":"app/img1.jpg","text":"Nice image","id":0},
-	  {"image":"app/img2.jpg","text":"Awesome photograph","id":1},
-	  {"image":"app/img3.jpg","text":"That is so cool","id":2},
-	  {"image":"app/img4.jpg","text":"I love that","id":3}]
+      this.slides = [{"image":"app/images/img1.jpg","text":"Nice image","id":0},
+	  {"image":"app/images/img2.jpg","text":"Awesome photograph","id":1},
+	  {"image":"app/images/img3.jpg","text":"That is so cool","id":2},
+	  {"image":"app/images/img4.jpg","text":"I love that","id":3}]
     }
   }
 
